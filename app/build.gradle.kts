38fc0e3fe96fb131415b2109ce2809b0e35f628f
Matheus Ferreira <matheus.ferreira@bandtec.com.br>
2024-12-23 16:32:02 -0300

    FEAT[libs][Navigation] Adiciona lib navigation do jetpack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 dependencies {
     implementation(libs.maps.compose)
     implementation(libs.coil.compose)
+    implementation(libs.navigation.compose)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
