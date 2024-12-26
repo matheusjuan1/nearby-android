44407e6ee5e685d4921982828277bb531dfd8fc9
Matheus Ferreira <matheus.ferreira@bandtec.com.br>
2024-12-26 18:38:31 -0300

    FEAT[libs][Navigation] Adiciona lib navigation do jetpack

--- app/src/main/java/com/matheus/juan/nearby/ui/screen/SplashScreen.kt
+++ app/src/main/java/com/matheus/juan/nearby/ui/screen/SplashScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -12,9 +13,15 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.matheus.juan.nearby.R
 import com.matheus.juan.nearby.ui.theme.GreenLight
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen(modifier: Modifier = Modifier) {
+fun SplashScreen(modifier: Modifier = Modifier, onNavigateToWelcome: () -> Unit) {
+    LaunchedEffect(key1 = Unit) {
+        delay(3_000)
+        onNavigateToWelcome()
+    }
+
     Box(
         modifier = modifier
             .background(GreenLight)
@@ -36,5 +43,5 @@ fun SplashScreen(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 private fun SplashScreenPreview() {
-    SplashScreen()
+    SplashScreen(onNavigateToWelcome = {})
 }
---
+++ app/src/main/java/com/matheus/juan/nearby/ui/screen/route/UIRoutes.kt
@@ -0,0 +1,9 @@
+package com.matheus.juan.nearby.ui.screen.route
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object Splash
+
+@Serializable
+data object Welcome
