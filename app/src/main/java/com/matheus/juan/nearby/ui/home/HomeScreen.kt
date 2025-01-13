d1541c972b6b3d780b786261650a9681b293f8c5
Matheus Ferreira <matheus.ferreira@bandtec.com.br>
2025-01-13 17:52:21 -0300

    FEAT[HomeScreen] Carregando pontos dos estabelecimentos no mapa

--- app/src/main/java/com/matheus/juan/nearby/ui/home/HomeScreen.kt
+++ app/src/main/java/com/matheus/juan/nearby/ui/home/HomeScreen.kt
@@ -13,17 +13,37 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.drawable.toBitmap
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.matheus.juan.nearby.R
 import com.matheus.juan.nearby.data.model.Market
+import com.matheus.juan.nearby.data.model.mock.mockUserLocation
 import com.matheus.juan.nearby.ui.component.category.NearbyCategoryFilterChipList
 import com.matheus.juan.nearby.ui.component.market.NearbyMarketCardList
 import com.matheus.juan.nearby.ui.theme.Gray100
+import com.matheus.juan.nearby.ui.util.findNortheastPoint
+import com.matheus.juan.nearby.ui.util.findSouthwestPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import okhttp3.internal.toImmutableList
+import kotlin.math.roundToInt
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -34,7 +54,6 @@ fun HomeScreen(
     onNavigateToMarketDetails: (Market) -> Unit
 ) {
     val bottomSheetState = rememberBottomSheetScaffoldState()
-    var isBottomSheetOpened by remember { mutableStateOf(true) }
 
     val configuration = LocalConfiguration.current
 
@@ -42,51 +61,141 @@ fun HomeScreen(
         onEvent(HomeUiEvent.OnFetchCategories)
     }
 
-    if (isBottomSheetOpened) {
-        BottomSheetScaffold(
-            modifier = modifier,
-            scaffoldState = bottomSheetState,
-            sheetContainerColor = Gray100,
-            sheetPeekHeight = configuration.screenHeightDp.dp * 0.5f, // BottomSheet terá tamanho inicial de metade da tela
-            sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
-            sheetContent = {
-                if (!uiState.markets.isNullOrEmpty()) {
-                    NearbyMarketCardList(
+    BottomSheetScaffold(
+        modifier = modifier,
+        scaffoldState = bottomSheetState,
+        sheetContainerColor = Gray100,
+        sheetPeekHeight = configuration.screenHeightDp.dp * 0.5f, // BottomSheet terá tamanho inicial de metade da tela
+        sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
+        sheetContent = {
+            if (!uiState.markets.isNullOrEmpty()) {
+                NearbyMarketCardList(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    markets = uiState.markets,
+                    onMarketClick = { selectedMarket ->
+                        onNavigateToMarketDetails(selectedMarket)
+                    }
+                )
+            }
+        },
+        content = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        bottom = it
+                            .calculateBottomPadding()
+                            .minus(8.dp)
+                    )
+            ) {
+                NearbyGoogleMap(
+                    modifier = Modifier.fillMaxSize(),
+                    uiState = uiState
+                )
+
+                if (!uiState.categories.isNullOrEmpty()) {
+                    NearbyCategoryFilterChipList(
                         modifier = Modifier
-                            .fillMaxSize()
-                            .padding(16.dp),
-                        markets = uiState.markets,
-                        onMarketClick = { selectedMarket ->
-                            onNavigateToMarketDetails(selectedMarket)
+                            .fillMaxWidth()
+                            .padding(top = 24.dp)
+                            .align(Alignment.TopStart),
+                        categories = uiState.categories,
+                        onSelectedCategoryChanged = { selectedCategory ->
+                            onEvent(HomeUiEvent.OnFetchMarkets(categoryId = selectedCategory.id))
                         }
                     )
                 }
-            },
-            content = {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(bottom = it.calculateBottomPadding().minus(8.dp))
-                ) {
-                    GoogleMap(
-                        modifier = Modifier.fillMaxSize()
-                    )
-
-                    if (!uiState.categories.isNullOrEmpty()) {
-                        NearbyCategoryFilterChipList(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(top = 24.dp)
-                                .align(Alignment.TopStart),
-                            categories = uiState.categories,
-                            onSelectedCategoryChanged = { selectedCategory ->
-                                onEvent(HomeUiEvent.OnFetchMarkets(categoryId = selectedCategory.id))
-                            }
-                        )
-                    }
-                }
             }
-        )
+        }
+    )
+}
+
+@Composable
+fun NearbyGoogleMap(modifier: Modifier = Modifier, uiState: HomeUiState) {
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+    val density = LocalDensity.current
+
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(mockUserLocation, 13f)
+    }
+    val uiSettings by remember {
+        mutableStateOf(MapUiSettings(zoomControlsEnabled = true))
+    }
+
+
+    GoogleMap(
+        modifier = modifier,
+        cameraPositionState = cameraPositionState,
+        uiSettings = uiSettings
+    ) {
+        context.getDrawable(R.drawable.ic_user_location)?.let {
+            Marker(
+                state = MarkerState(position = mockUserLocation),
+                icon = BitmapDescriptorFactory.fromBitmap(
+                    it.toBitmap(
+                        width = density.run { 72.dp.toPx() }.roundToInt(),
+                        height = density.run { 72.dp.toPx() }.roundToInt()
+                    )
+                )
+            )
+        }
+
+        if (!uiState.markets.isNullOrEmpty()) {
+            context.getDrawable(R.drawable.img_pin)?.let {
+                uiState.marketLocation?.toImmutableList()
+                    ?.forEachIndexed { index, location ->
+                        Marker(
+                            state = MarkerState(position = location),
+                            icon = BitmapDescriptorFactory.fromBitmap(
+                                it.toBitmap(
+                                    width = density.run { 36.dp.toPx() }
+                                        .roundToInt(),
+                                    height = density.run { 36.dp.toPx() }
+                                        .roundToInt()
+                                )
+                            ),
+                            title = uiState.markets[index].name
+                        )
+                    }.also {
+                        coroutineScope.launch {
+                            val allMarks = uiState.marketLocation?.plus(
+                                mockUserLocation
+                            )
+
+                            val southwestPoint =
+                                findSouthwestPoint(points = allMarks.orEmpty())
+                            val northeastPoint =
+                                findNortheastPoint(points = allMarks.orEmpty())
+
+                            val centerPointLatitude =
+                                (southwestPoint.latitude + northeastPoint.latitude) / 2
+                            val centerPointLongitude =
+                                (southwestPoint.longitude + northeastPoint.longitude) / 2
+
+                            val cameraUpdate =
+                                CameraUpdateFactory.newCameraPosition(
+                                    CameraPosition(
+                                        LatLng(
+                                            centerPointLatitude,
+                                            centerPointLongitude
+                                        ),
+                                        13f,
+                                        0f,
+                                        0f
+                                    )
+                                )
+                            delay(200)
+                            cameraPositionState.animate(
+                                cameraUpdate,
+                                durationMs = 500
+                            )
+                        }
+                    }
+            }
+        }
     }
 }
 
